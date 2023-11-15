06befb0d56bcd312591589766da48956b5247c73
Upegui2005 <emmaupegui2005@gmail.com>
2023-11-15 15:16:58 -0500

    2.1.1

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-console.log("Mi primer proyecto Node")
-
 const express = require('express')
 const app = express()
-const port = 3030
+const port = 3031
+const path = require('path');
 
-app.use(express.static('/assets'))
+app.use('/assets', express.static(path.join(__dirname, 'assets')))
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {

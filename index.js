7dca82a01edfb53c0fd86ad13c6ca38809f44d22
Upegui2005 <emmaupegui2005@gmail.com>
2023-11-15 13:34:55 -0500

    2.1.0

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const express = require('express')
 const app = express()
 const port = 3030
 
+app.use(express.static('/assets'))
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('Holi');
+    res.render('catalogo');
 })
 
 app.listen(port, () => {
